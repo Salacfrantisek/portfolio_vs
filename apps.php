3d16c476d1eab7624aa36d554e311767826642db
Salacfrantisek <salacfrantisek@seznam.cz>
2021-06-15 12:41:31 +0200

    php

--- apps.php
+++ apps.php
@@ -7,7 +7,7 @@
     <meta name="description" content="Portfolio začínajícího programátora(?)">
     <meta name="author" content="František Salač">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>František Salač - Portfolio - Služby</title>
+    <title>František Salač - Portfolio - Aplikace</title>
     <link rel="stylesheet" href="theme.css">
 </head>
 
@@ -22,8 +22,9 @@
     <br>
 
     <center>
-        <h1>Služby - Work in progess </h1>
+        <h1>Aplikace - Work in progess </h1>
     </center>
+    <?php include 'php/botbar.php'; ?>
 </body>
 
 </html>
---
+++ php/botbar.php
@@ -0,0 +1,8 @@
+<!-- Created by, etc.. -->
+<div class="bottomText">
+
+    <p> know a css file can control the format of many html pages - is there an analogy to this that can control the content of many html pages? </p>
+
+
+
+</div>
